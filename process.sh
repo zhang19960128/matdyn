8493f91b629a8c600d69f89e411e9a56d07a97cf
Bruce_zhang <zjh199601283492@gmail.com>
2018-06-03 16:32:42 -0400

    first

--- process.sh
+++ process.sh
@@ -4,3 +4,14 @@ file2="bzo.dyn1"
 grep -m1 -A 500 "\*" $file | grep -v "\*" | grep -v "freq (" | sed "s/""(""//g" | sed "s/"")""//g" >eigev.txt
 grep -m1 -A 500 "Effective Charges" $file2 | tac |grep -m1 -A 500 "Effective Charges" | tac | grep -v "Effective Charges" | grep -v "atom" >atomeffect.txt
 sed -i '/^$/d' atomeffect.txt
+count=`grep "freq (" matdyn.modes | grep -Eo "[\-]{0,1}[0-9]{0,9}[\.]{1}[0-9]{1,10}" | wc -l`;
+count=`expr $count \/ 2`;
+usefulline="";
+for i in `seq 1 $count`
+do
+    temp=`expr 2 \* $i`;
+    usefulline=$usefulline"${temp}p;";
+done
+grep "freq (" matdyn.modes | grep -Eo "[\-]{0,1}[0-9]{0,9}[\.]{1}[0-9]{1,10}" >temp.txt;
+sed -n $usefulline temp.txt >fre.txt
+rm temp.txt
