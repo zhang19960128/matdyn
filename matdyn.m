ab3f4a09e4bb09beb5eaf6d8e2835c482c9c03a3
Bruce_zhang <zjh199601283492@gmail.com>
2018-06-10 14:48:09 -0400

    first

--- matdyn.m
+++ matdyn.m
@@ -15,18 +15,21 @@ for i=atom/5*4+1:atom
 end;
 vol=(8.386494280/2)^3;
 for mu=1:atom*3
-a_mu=renorm(eigvector(atom*(mu-1)+1:atom*(mu-1)+atom,1:end),mass);
-modeff=zeros(3,1);
-for i=1:atom
-    i_born=atomborn(3*(i-1)+1:3*(i-1)+3,1:end);
-    a_mu_i=a_mu(i,1:end);
-    contr_i=i_born*(a_mu_i')/sqrt(mass(i));
-    modeff=modeff+contr_i;
-end
-for j=1:3
-    fprintf(fileID,"%f ",modeff(j));
-end
-fprintf(fileID,'\n');
+    %%%%%%%%use the following line when you are using mode displacement%%%%%%%%%%%%%%%%%%%%%%%%%%
+    a_mu=renorm(eigvector(atom*(mu-1)+1:atom*(mu-1)+atom,1:end),mass);%%%%%%%%%%%%%%%%
+    %%%%%%%%%%%%%%%use this line when you are using eigen vector of the dynamic matrix%%%%%%%%%%
+    %a_mu=eigvector(atom*(mu-1)+1:atom*(mu-1)+atom,1:end);
+    modeff=zeros(3,1);
+    for i=1:atom
+        i_born=atomborn(3*(i-1)+1:3*(i-1)+3,1:end);
+        a_mu_i=a_mu(i,1:end);
+        contr_i=i_born*(a_mu_i')/sqrt(mass(i));
+        modeff=modeff+contr_i;
+    end
+    for j=1:3
+        fprintf(fileID,"%f ",modeff(j));
+    end
+    fprintf(fileID,'\n');
 end
 borne=importdata("modeeffect.txt");
 freq=importdata("fre.txt");
--- process.sh
+++ process.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-file="pbematdyn.modes"
-file2="pbebzo.dyn1"
+file="matdyn.modes"
+file2="bzo.dyn1"
 grep -m1 -A 500 "\*" $file | grep -v "\*" | grep -v "freq (" | sed "s/""(""//g" | sed "s/"")""//g" >eigev.txt
 grep -m1 -A 500 "Effective Charges" $file2 | tac |grep -m1 -A 500 "Effective Charges" | tac | grep -v "Effective Charges" | grep -v "atom" >atomeffect.txt
 sed -i '/^$/d' atomeffect.txt
